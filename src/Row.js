2b0c2b37ad059c36323abdf00e4d82b2368bacce
Rytis Želvys <rytis6161@gmail.com>
2020-11-23 17:37:06 +0200

    migrations working
    img in page!!!

--- src/Row.js
+++ src/Row.js
@@ -1,30 +1,35 @@
 import React, { useEffect, useState} from 'react';
 import axios from './axios';
 
+const base_url = "https://image.tmdb.org/t/p/w500/";
 
 function Row({ title, fetchURL }) {
     const [movies, setMovies] = useState ([]);
 //
     useEffect (() => {
 //
-      async function fetchData() {
+async function fetchData() {
 const request = await axios.get(fetchURL);
 console.log(request);
+setMovies (request.data.results);
 return request;
       }  
       fetchData();
-    }, []);
-
-
-
+    }, [fetchURL]);
 
     return (
-        <div>
+        <div className="row">
            <h2> {title} </h2>
 
+        <div className="row_posters">
 {/*container >>posters*/}
-
+{movies.map(movie => (
+    <img 
+    className="row_poster"
+    src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+    ))}
         </div>
+     </div>
     )
 }
 
