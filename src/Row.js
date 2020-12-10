7f5d584b93039e779eca8522e34e2438a434d9b3
Rytis Želvys <rytis6161@gmail.com>
2020-12-10 19:47:40 +0200

    Update Row.js

--- src/Row.js
+++ src/Row.js
@@ -30,28 +30,29 @@ return request;
         },
       };
 
-const handleClick = (movie) => {
-  if (trailerUrl)  {
-      setTrailerUrl('');
-  } else {
-      movieTrailer (movie?.name || "")
-      .then(url => {
-          const urlParams = new URLSearchParams (new URL(url).search);
-          setTrailerUrl(urlParams.get('v'));
-      })
-      .catch((error) => console.log(error));
-  }
-}
+    function handleClick(movie) {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "")
+                .then(url => {
+                    //https://www.youtube.com/watch?v=a6_TXbDgHro&ab_channel=NegyvasEteris
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                })
+                .catch((error) => console.log(error));
+        }
+    }
 
     return (
         <div className="row">
            <h2> {title} </h2>
 
         <div className="row_posters">
-{/*container >>posters*/}
 {movies.map(movie => (
     <img 
     key={movie.id}
+    onClick={() => handleClick(movie)}
     className={`row_poster ${isLargeRow && "row__posterLarge"}`}
     src={`${base_url}${
         isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
